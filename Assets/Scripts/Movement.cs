5cad3a6e62d08b9adf8d31feb166bb87dbfdf267
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2023-05-31 23:58:16 +0200

    created movement script(non-working)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float sprintSpeed = 7f;
+    public float jumpHeight = 2f;
+
+    public Transform camera;
+
+    void Update()
+    {
+        
+    }
+}
