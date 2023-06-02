4ae5f0ce600190cfd2e9ba050ae9d00f0f67c0b6
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2023-06-02 23:23:54 +0200

    Movement working (kinda B)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float sprintSpeed = 7f;
+    public float speed = 25f;
+    public float sprintSpeed = 35f;
     public float jumpHeight = 2f;
+    public int dragOnGround = 2; 
+    public int dragInAir = 1;
+    bool grounded = false;
 
     Rigidbody charRb;
+    Collider sphereCollider;
     public Transform camera;
 
     void Start() 
     {
         charRb = GetComponent<Rigidbody>();
+        sphereCollider = gameObject.transform.GetChild(0).GetComponent<Collider>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
 
         Vector3 charMovement = (transform.forward * vertical + transform.right * horizontal).normalized;
-        charRb.velocity = charMovement * Time.deltaTime * speed;
+        charRb.AddForce(charMovement * speed, ForceMode.Force);
+
+        
+        if (Input.GetButton("Jump") && grounded) 
+        {
+            grounded = false;
+            charRb.drag = dragInAir;
+            charRb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        grounded = true;
+        charRb.drag = dragOnGround;
     }
 }
