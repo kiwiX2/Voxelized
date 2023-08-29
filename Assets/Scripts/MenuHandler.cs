38f34ee09be6b5ec024ba6e284b286d781f44d2d
HenrikB <92089215+henrikboren@users.noreply.github.com>
2023-08-29 14:04:33 +0200

    create prefab (not a tree)

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -2,9 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MenuHandler : MonoBehaviour
 {
+    public Material woodMaterial;
+    
+    public void GrowTrees()
+    {
+        Vector3 position = new Vector3(1, 1, 1);
+
+        GameObject empty = new GameObject("Tree");
+        GameObject voxelObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Renderer renderer = voxelObject.GetComponent<Renderer>();
+        Material material = renderer.material;
+
+        voxelObject.transform.position = position;
+        renderer.material = woodMaterial;
+        material.SetFloat("_Smoothness", 0.2f);
+        voxelObject.transform.SetParent(empty.transform);
+
+        PrefabUtility.SaveAsPrefabAsset(empty, "Assets/Prefabs/Tree.prefab");
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game Scene");
